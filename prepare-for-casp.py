0f1ef90c31612e5f417eba33cbe0d82dfc8c09a4
GoldEagle93 <63361598+GoldEagle93@users.noreply.github.com>
2022-06-09 19:27:03 -0400

    Update prepare-for-casp.py

--- prepare-for-casp.py
+++ prepare-for-casp.py
@@ -27,21 +27,24 @@ with open('submission.pdb', 'w') as outputfile:
         for x in temp:
             xx = x.split()
         # for x in temp[8200:8230]:
-            if xx[0] == 'TER' or 'END' in xx[0]:
+            if xx[0] == 'TER':
                 outfile.append(x)
                 continue
             for y in model:
                 yy = y.split()
-                if yy[-1] == 'H' or yy[0] == 'TER' or 'END' in yy[0] or 'CRYST' in y:
+                if yy[-1] == 'H' or yy[0] == 'TER' or 'END' in yy[0] or 'CRYST' in y or  'END' in xx:
                     continue
                 elif xx[4] == '0' and int(xx[5])+0 == int(yy[4]) and xx[2] == yy[2]:
                     outfile.append(
                     x[:30] + y[30:56] + x[56:60] + '  ' + bf[yy[4]][:4] + x[66:]
                     )
-                elif xx[4] == '1' and int(xx[5])+69 == int(yy[4]) and xx[2] == yy[2]:
-                    outfile.append(
-                    x[:30] + y[30:56] + x[56:60] + '  ' + bf[yy[4]][:4] + x[66:]
-                    )
+        outputfile.write('MODEL %s\n'%str(int(index)+1))
+        outputfile.write('PARENT N/A\n')
+        outputfile.write('ATOM      1  P     G 0   1       0.000   0.000   0.000  1.00  0.00           P\n')
+        outputfile.write('ATOM      2  OP1   G 0   1       0.000   0.000   0.000  1.00  0.00           O\n')
+        outputfile.write('ATOM      3  OP2   G 0   1       0.000   0.000   0.000  1.00  0.00           O\n')
         for line in outfile:
             outputfile.write(line)
+        outputfile.write('ENDMDL\n')
+    outputfile.write('END\n')
 outputfile.close()
